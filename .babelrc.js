0b03e54ea7989d1c8bee0e5c9bb06680e781ab3f
Nguyen Phat <phatnt.uit@gmail.com>
2020-07-30 20:40:27 +0700

    Update babel configuration

---
+++ .babelrc.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  presets: [
+    '@babel/preset-typescript',
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ts'],
+      },
+    ],
+  ],
+};
